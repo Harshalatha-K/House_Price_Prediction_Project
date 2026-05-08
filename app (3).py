f3712bba28c91b3efe9b18a350ac3dc04d914ca9
Harshalatha-K <harshalatha.22cse@cambridge.edu.in>
2026-05-08 20:36:28 +0530

    Add files via upload

---
+++ app (3).py
@@ -0,0 +1,40 @@
+
+import streamlit as st
+import numpy as np
+import joblib
+
+# Load trained model
+model = joblib.load('model.pkl')
+
+# Title
+st.title("House Price Prediction")
+
+st.write("Enter House Details")
+
+# Inputs
+area = st.number_input("Area", min_value=500)
+
+bedrooms = st.number_input("Bedrooms", min_value=1)
+
+bathrooms = st.number_input("Bathrooms", min_value=1)
+
+stories = st.number_input("Stories", min_value=1)
+
+parking = st.number_input("Parking", min_value=0)
+
+# Prediction Button
+if st.button("Predict Price"):
+
+    input_data = np.array([[
+        area,
+        bedrooms,
+        bathrooms,
+        stories,
+        parking
+    ]])
+
+    prediction = model.predict(input_data)
+
+    st.success(
+        f"Predicted House Price: ₹ {prediction[0]:,.2f}"
+    )
